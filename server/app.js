e71f7f3a109ed05ff77d269f54b3fff373097c1a
AlieuBSagnia <alieubsagnia@gmail.com>
2021-05-15 22:00:23 +0100

    added cors

--- server/app.js
+++ server/app.js
@@ -5,14 +5,12 @@ const meta = require("./content/meta.json");
 const app = express();
 const article = require("./content/article.json");
 const port = 3000;
-const cors = require("cors");
 
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 app.set('meta', meta);
 app.set("article", article);
 app.use('*/static', express.static('public'));
-app.use(cors());
 
 app.use(router);
 
