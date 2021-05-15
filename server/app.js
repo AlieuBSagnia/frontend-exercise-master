beb61bf5badefe2da56e778cb3f074451aa06e8e
AlieuBSagnia <alieubsagnia@gmail.com>
2021-05-15 22:31:29 +0100

    added post.json to app

--- server/app.js
+++ server/app.js
@@ -4,12 +4,14 @@ const router = require('./router');
 const meta = require("./content/meta.json");
 const app = express();
 const article = require("./content/article.json");
+const posts = require("./content/posts.json");
 const port = 3000;
 
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 app.set('meta', meta);
 app.set("article", article);
+app.set("posts", posts);
 app.use('*/static', express.static('public'));
 
 app.use(router);
