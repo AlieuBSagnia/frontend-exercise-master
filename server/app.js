6c00b882adacf617aca35fd1407dfecfaa73662a
AlieuBSagnia <alieubsagnia@gmail.com>
2021-05-15 22:46:57 +0100

    Added comments to app.js

--- server/app.js
+++ server/app.js
@@ -5,6 +5,7 @@ const meta = require("./content/meta.json");
 const app = express();
 const article = require("./content/article.json");
 const posts = require("./content/posts.json");
+const comments = require("../db.json");
 const port = 3000;
 
 app.engine('handlebars', exphbs());
@@ -12,6 +13,7 @@ app.set('view engine', 'handlebars');
 app.set('meta', meta);
 app.set("article", article);
 app.set("posts", posts);
+app.set("comments", comments);
 app.use('*/static', express.static('public'));
 
 app.use(router);
