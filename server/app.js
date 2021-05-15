8994a33d63696c550fdae09d7e45823dc7ef92ea
AlieuBSagnia <alieubsagnia@gmail.com>
2021-05-15 14:31:45 +0100

    added article.json to app.js

--- server/app.js
+++ server/app.js
@@ -3,13 +3,16 @@ const exphbs  = require('express-handlebars');
 const router = require('./router');
 const meta = require("./content/meta.json");
 const app = express();
+const article = require("./content/article.json");
 const port = 3000;
+const cors = require("cors");
 
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 app.set('meta', meta);
-
+app.set("article", article);
 app.use('*/static', express.static('public'));
+app.use(cors());
 
 app.use(router);
 
