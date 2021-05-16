e0ec9d1d89636577135170fa92917263640e2c5d
AlieuBSagnia <alieubsagnia@gmail.com>
2021-05-16 01:23:20 +0100

    added cors in app.js

--- server/app.js
+++ server/app.js
@@ -7,6 +7,7 @@ const article = require("./content/article.json");
 const posts = require("./content/posts.json");
 const comments = require("../db.json");
 const moment = require("moment");
+const cors = require("cors");
 const port = 3000;
 var hbs = exphbs.create({
 	
@@ -24,7 +25,7 @@ app.set("article", article);
 app.set("posts", posts);
 app.set("comments", comments);
 app.use('*/static', express.static('public'));
-
+app.use(cors());
 app.use(router);
 
 app.listen(port, () => console.log(`Listening on port ${port}!`));
